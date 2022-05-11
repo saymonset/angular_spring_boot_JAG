0b8cdbd24aaea5beb950cbf32152af91f985b0fd
Simon A. Rodriguez P <saymon_set@hotmail.com>
2022-05-11 08:13:45 -0400

    Seccion4->42.Aqui con map , casteamos el tipo a arreglo de clientesen angular

--- seccion4-backend-rest/clientes-app/src/app/clientes/cliente.service.ts
+++ seccion4-backend-rest/clientes-app/src/app/clientes/cliente.service.ts
@@ -3,6 +3,7 @@ import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+//debe estar registrada en el modulo (HttpClientModule)
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
@@ -14,6 +15,7 @@ export class ClienteService {
 
   getClientes(): Observable<Cliente[]> {
     //return of(CLIENTES);
+    //Aqui con map , casteams el tipo a arreglo de clientes 
     return this.http.get(this.urlEndPoint).pipe(
       map(response => response as Cliente[])
     );
