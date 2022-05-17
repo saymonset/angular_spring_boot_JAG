592c1fe941d907efac45b86ed097501ec0747f2f
Simon A. Rodriguez P <saymon_set@hotmail.com>
2022-05-17 05:18:12 -0400

    Seccion15->OAuth2-jwt

---
+++ seccion15-oauth2-jwt/spring-boot-backend-apirest[OAuth2_Anotacion_Secured]/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/auth/ResourceServerConfig.java
@@ -0,0 +1,24 @@
+package com.bolsadeideas.springboot.backend.apirest.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**").permitAll()
+		/*.antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("USER", "ADMIN")
+		.antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("USER", "ADMIN")
+		.antMatchers(HttpMethod.POST, "/api/clientes").hasRole("ADMIN")
+		.antMatchers("/api/clientes/**").hasRole("ADMIN")*/
+		.anyRequest().authenticated();
+	}
+
+	
+}
